d98cbbb508650fc021715f237558fdb0aa539dc8
huypham <huyphuocbinh204@gmail.com>
2025-04-17 19:09:39 +0700

    ISSUSE-3: fix

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-const { isEven } = require('./index');
+const { isEven, isOdd } = require('./index');
 
 console.log(isEven(4));
 console.log(isEven(7)); 
 
-console.log("isOdd(4):", isOdd(4));
-console.log("isOdd(7):", isOdd(7)); 
+console.log(isOdd(4));
+console.log(isOdd(7)); 
